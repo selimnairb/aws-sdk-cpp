f73063e095f0ac1c008067578e4be20374b71711
sbiscigl <sbiscigl@amazon.com>
2022-07-06 13:50:14 -0400

    Adds define for gtest on old gcc releases.

--- testing-resources/include/aws/external/gtest/gtest-matchers.h
+++ testing-resources/include/aws/external/gtest/gtest-matchers.h
@@ -48,6 +48,7 @@
 #include "gtest/gtest-printers.h"
 #include "gtest/internal/gtest-internal.h"
 #include "gtest/internal/gtest-port.h"
+#include "gtest/gtest-aws-helper.h"
 
 // MSVC warning C5046 is new as of VS2017 version 15.8.
 #if defined(_MSC_VER) && _MSC_VER >= 1915
